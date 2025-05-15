a57d41aefe5062e508c220b39e0ffef8b35f67fd
GhouseJohn <md.gosoddin@gmail.com>
2025-05-15 20:26:13 +0530

    Teamplate Design Pattern

---
+++ TemplateExam/TemplateExam/BasicExample/DataExport.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace TemplateExam.BasicExample
+{
+    public abstract class DataExport
+    {
+        protected DataExport()
+        {
+            ReadData();
+            ProcessData();
+            ExportData();
+        }
+        public abstract void ReadData();
+        public virtual void ProcessData()
+        {
+            Console.WriteLine("Processing data...");
+        }
+        public virtual void ExportData()
+        {
+            Console.WriteLine("Exporting data...");
+        }
+    }
+
+    public class ExcelExport : DataExport
+    {
+        public override void ReadData()
+        {
+            Console.WriteLine("Reading data from Excel file...");
+        }
+        public override void ExportData()
+        {
+            Console.WriteLine("Exporting data to Excel file...");
+        }
+    }
+    public class CsvExport : DataExport
+    {
+        public override void ReadData()
+        {
+            Console.WriteLine("Reading data from CSV file...");
+        }
+        public override void ExportData()
+        {
+            Console.WriteLine("ExportData data to CSV file...");
+        }
+    }
+}
---
+++ TemplateExam/TemplateExam/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TemplateExam.BasicExample;
+
+Console.WriteLine("Hello, World!");
+//Data Process
+DataExport dataExport = new ExcelExport();
+dataExport.ReadData();
+dataExport.ProcessData();
+
+DataExport dataExport2 = new CsvExport();
+dataExport2.ReadData();
+dataExport2.ExportData();
